a30821737fa612cb9a6bf5de9cc1361011d63daf
Tomo <94550718+Tomo1151@users.noreply.github.com>
2025-04-08 22:49:10 +0900

    Fix LD (a16), SP test to use correct address for memory write

--- emu/src/test/java/com/syntck/cpu/LDTest.java
+++ emu/src/test/java/com/syntck/cpu/LDTest.java
@@ -194,11 +194,11 @@ public class LDTest {
   public void testLDa16SP() throws Exception {
     CPU cpu = new CPU();
     cpu.bus.writeByte(0x0000, 0x08); // LD (a16), SP
-    cpu.bus.writeByte(0x0001, 0x00);
-    cpu.bus.writeByte(0x0002, 0xC0); // アドレス0xC000
+    cpu.bus.writeByte(0x0001, 0x02);
+    cpu.bus.writeByte(0x0002, 0xC0); // アドレス0xC002
     cpu.sp = 0x1234;
     cpu.step();
-    assertEquals(0x1234, cpu.bus.readWord(0xC000)); // SPの値がメモリに書き込まれる
+    assertEquals(0x1234, cpu.bus.readWord(0xC002)); // SPの値がメモリに書き込まれる
     assertEquals(0x0003, cpu.pc);
   }
 }
